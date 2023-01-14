d1fead7555c8a74110e4c2671f1d563f7ff5a7ac
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-01-14 20:02:20 -0300

    Added working get_data()

--- indice_vendas/indice_vendas.py
+++ indice_vendas/indice_vendas.py
@@ -6,6 +6,51 @@ import xlsxwriter
 
 def main():
     period = sidra_helpers.get_period(config.START_DATE)
+    sidra_data = get_data(period)
+
+
+def get_data(period: str) -> list[list]:
+    t8185 = sidrapy.get_table(
+        table_code="8185", 
+        territorial_level="1",
+        ibge_territorial_code="1",
+        variable="11707",
+        header="n",
+        format="list",
+        period=period,
+        classifications = {'11046': '56734'}
+    )
+    t8186 = sidrapy.get_table(
+        table_code="8186", 
+        territorial_level="1",
+        ibge_territorial_code="1",
+        variable="11707",
+        header="n",
+        format="list",
+        period=period,
+        classifications = {'11046': '56736'}
+    )
+    t8159 = sidrapy.get_table(
+        table_code="8159", 
+        territorial_level="1",
+        ibge_territorial_code="1",
+        variable="11600",
+        header="n",
+        format="list",
+        period=period,
+        classifications = {'544': '129314'}
+    )
+    t8161 = sidrapy.get_table(
+        table_code="8161", 
+        territorial_level="1",
+        ibge_territorial_code="1",
+        variable="11622",
+        header="n",
+        format="list",
+        period=period,
+        classifications = {'11046': '56736'}
+    )
+    return [t8185, t8186, t8159, t8161]
 
 
 if __name__=="__main__":
