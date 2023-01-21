1311aed4becc40746bb1ab5159679edabcdc8593
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-01-21 20:18:32 -0300

    Working final version

--- indice_vendas/config.py
+++ indice_vendas/config.py
@@ -1,3 +1,27 @@
-START_DATE = "2012-01-01"
+SERIES_START_DATE = "2012-01-01"
+CHART_START_DATE = "2018-01-01"
 
 FILE_PATH = "../files/"
+
+x_axis = {
+    'num_font': {'name': 'Calibri', 'size': 12},
+    'date_axis': True,
+    'num_format': 'mmm-yy',
+    'label_position': 'low',
+    'major_unit': 2,
+    'major_unit_type': 'months',
+}
+
+y_axis = {
+    'num_font': {'name': 'Calibri', 'size': 12},
+    'min': 60,
+    'max': 100,
+    'major_unit': 5,
+    'major_gridlines': {'visible': False},
+}
+
+legend = {
+    'position': 'bottom',
+    'font': {'name': 'Calibri', 'size': 12},
+    'delete_series': [4],
+}
--- indice_vendas/indice_vendas.py
+++ indice_vendas/indice_vendas.py
@@ -5,7 +5,7 @@ import xlsxwriter
 
 
 def main():
-    period = sidra_helpers.get_period(config.START_DATE)
+    period = sidra_helpers.get_period(config.SERIES_START_DATE)
     sidra_data = get_data(period)
     sidra_data = sidra_helpers.api_to_list(sidra_data)
     headers = ['Mês', 'Varejo', 'Varejo ampliado', 'Indústria', 'Serviços']
@@ -18,6 +18,7 @@ def main():
         "https://github.com/GuilhermeFrainer/graficos_excel",
         "Fontes dos dados: API do Sidra, tabelas 8185, 8186, 8159 e 8161"
     ]    
+    make_chart(workbook)
     sidra_helpers.make_credits(workbook, credits)
     workbook.close()
 
@@ -64,6 +65,89 @@ def get_data(period: str) -> list[list]:
         classifications = {'11046': '56726'}
     )
     return [t8185, t8186, t8159, t8161]
+
+
+def make_chart(workbook: xlsxwriter.Workbook) -> None:
+    series_size = sidra_helpers.get_series_size()
+    chart_start = find_chart_start()
+    chartsheet = workbook.add_chartsheet('Gráfico')
+    
+    chart = workbook.add_chart({'type': 'line'})
+    chart.add_series({
+        # Varejo
+        'name': '=Dados!$G$5',
+        'categories': f'=Dados!$A${chart_start}:$A${5 + series_size}',
+        'values': f'=Dados!$G${chart_start}:$G${5 + series_size}',
+        'line': {'color': '#c00000'},
+        'data_labels': {
+            'num_format': '0.0',
+            'font': {
+                'color': '#c00000',
+                'size': 12,
+            }
+        },
+    })
+    chart.add_series({
+        # Varejo ampliado
+        'name': '=Dados!$H$5',
+        'categories': f'=Dados!$A${chart_start}:$A${5 + series_size}',
+        'values': f'=Dados!$H${chart_start}:$H${5 + series_size}',
+        'line': {'color': '#4c7ac6'},
+        'data_labels': {
+            'num_format': '0.0',
+            'font': {
+                'color': '#4c7ac6',
+                'size': 12,
+            }
+        },
+    })
+    chart.add_series({
+        # Indústria
+        'name': '=Dados!$I$5',
+        'categories': f'=Dados!$A${chart_start}:$A${5 + series_size}',
+        'values': f'=Dados!$I${chart_start}:$I${5 + series_size}',
+        'line': {'color': '#75ac46'},
+        'data_labels': {
+            'num_format': '0.0',
+            'font': {
+                'color': '#75ac46',
+                'size': 12,
+            }
+        },
+    })
+    chart.add_series({
+        # Serviços
+        'name': '=Dados!$J$5',
+        'categories': f'=Dados!$A${chart_start}:$A${5 + series_size}',
+        'values': f'=Dados!$J${chart_start}:$J${5 + series_size}',
+        'line': {'color': '#f7c722'},
+        'data_labels': {
+            'num_format': '0.0',
+            'font': {
+                'color': '#f7c722',
+                'size': 12,
+            }
+        },
+    })
+    chart.add_series({
+        # 100
+        'name': '=Dados!$K$5',
+        'categories': f'=Dados!$A${chart_start}:$A${5 + series_size}',
+        'values': f'=Dados!$K${chart_start}:$K${5 + series_size}',
+        'line': {'color': '#000000'},
+    })
+    chart.set_x_axis(config.x_axis)
+    chart.set_y_axis(config.y_axis)
+    chart.set_legend(config.legend)
+    chartsheet.set_chart(chart)
+
+
+# Returns chart starting point (in the Excel file) by calculating the differnece (in months) between the chart starting point and the series'
+def find_chart_start() -> int:
+    series_start = config.SERIES_START_DATE.split("-")
+    chart_start = config.CHART_START_DATE.split("-")
+    difference = (int(chart_start[0]) - int(series_start[0])) * 12 + (int(chart_start[1]) - int(series_start[1]))
+    return difference + 6
 
 
 if __name__=="__main__":
