716d7b8a7a3972d085cf4ea7d3adc229331e613e
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-01-25 12:56:44 -0300

    Working version of indice_volume.py

--- indice_volume/config.py
+++ indice_volume/config.py
@@ -1,3 +1,25 @@
 START_DATE = '2018-01-01'
 
+x_axis = {
+    'num_font': {'name': 'Calibri', 'size': 12},
+    'date_axis': True,
+    'num_format': 'mmm-yy',
+    'label_position': 'low',
+    'major_unit': 1,
+    'major_unit_type': 'months',
+}
+
+y_axis = {
+    'num_font': {'name': 'Calibri', 'size': 12},
+    'min': -10,
+    'max': 15,
+    'major_unit': 5,
+    'major_gridlines': {'visible': False},
+}
+
+legend = {
+    'position': 'bottom',
+    'font': {'name': 'Calibri', 'size': 12},
+}
+
 FILE_PATH = "../files/"
--- indice_volume/indice_volume.py
+++ indice_volume/indice_volume.py
@@ -1,26 +1,29 @@
 from sidrapy import get_table
-from sidra_helpers import get_period, api_to_list, make_excel
-import config
+import sidra_helpers
 import xlsxwriter
+import config
 
 
 def main():
-    
-    period = get_period(config.START_DATE)
+    period = sidra_helpers.get_period(config.START_DATE)
     api_data = get_data(period)
-    api_data = api_to_list(api_data)
-    headers = ['Varejo', 'Varejo Ampliado', 'Indústria', 'Serviços']
-    workbook, worksheet = make_excel(f'{config.FILE_PATH}Índice volume', api_data, headers)
+    api_data = sidra_helpers.api_to_list(api_data)
+    headers = ['Mês', 'Varejo', 'Varejo Ampliado', 'Indústria', 'Serviços']
+    workbook, worksheet = sidra_helpers.make_excel(f'{config.FILE_PATH}Índice volume', api_data, headers)
+    make_charts(workbook)
 
-
+    credits = [
+        'Arquivo feito em Python. Link do código:',
+        'https://github.com/GuilhermeFrainer/graficos_excel',
+        'Fontes dos dados: tabelas 8185, 8186, 8159 e 8161 do SIDRA',
+    ]
+    sidra_helpers.make_credits(workbook, credits)
     workbook.close()
     
 
-
 # Gets data from the Sidra API
 def get_data(period: str) -> list[list]:
-
-    retail = get_table(
+    t8185 = get_table(
         table_code="8185", 
         territorial_level="1",
         ibge_territorial_code="1",
@@ -30,41 +33,104 @@ def get_data(period: str) -> list[list]:
         period=period,
         classifications={"11046": "56734"},
     )
-
-    ext_retail = get_table(
+    t8186 = get_table(
         table_code="8186", 
         territorial_level="1",
         ibge_territorial_code="1",
-        variable="11707",
+        variable="11711",
         header="n",
         format="list",
         period=period,
         classifications={"11046": "56736"},
     )
-
-    industry = get_table(
+    t8159 = get_table(
         table_code="8159", 
         territorial_level="1",
         ibge_territorial_code="1",
-        variable="11600",
+        variable="11604",
         header="n",
         format="list",
         period=period,
         classifications={"544": "129314"},
     )
-
-    services = get_table(
-        table_code="8159", 
+    t8161 = get_table(
+        table_code="8161", 
         territorial_level="1",
         ibge_territorial_code="1",
-        variable="11622",
+        variable="11626",
         header="n",
         format="list",
         period=period,
         classifications={"11046": "56726"},
     )
+    return [t8185, t8186, t8159, t8161]
 
-    return [retail, ext_retail, industry, services]
+
+def make_charts(workbook: xlsxwriter.Workbook) -> None:
+    series_size = sidra_helpers.get_series_size()
+    chart = workbook.add_chart({'type': 'line'})
+
+    chart.add_series({
+        # Varejo
+        'name': '=Dados!$B$1',
+        'categories': f'=Dados!$A$2:$A${series_size + 2}',
+        'values': f'=Dados!$B$2:$B${series_size + 2}',
+        'line': {'color': '#c30c0e'},
+        'data_labels': {
+            'font': {
+                'color': '#c30c0e',
+                'size': 12,
+            },
+        },
+    })
+    chart.add_series({
+        # Varejo ampliado
+        'name': '=Dados!$C$1',
+        'categories': f'=Dados!$A$2:$A${series_size + 2}',
+        'values': f'=Dados!$C$2:$C${series_size + 2}',
+        'line': {'color': '#0474c4'},
+        'data_labels': {
+            'font': {
+                'color': '#0474c4',
+                'size': 12,
+            },
+        },
+    })
+    chart.add_series({
+        # Indústria
+        'name': '=Dados!$D$1',
+        'categories': f'=Dados!$A$2:$A${series_size + 2}',
+        'values': f'=Dados!$D$2:$D${series_size + 2}',
+        'line': {'color': '#0db260'},
+        'data_labels': {
+            'font': {
+                'color': '#0db260',
+                'size': 12,
+            },
+        },
+    })
+    chart.add_series({
+        # Serviços
+        'name': '=Dados!$E$1',
+        'categories': f'=Dados!$A$2:$A${series_size + 2}',
+        'values': f'=Dados!$E$2:$E${series_size + 2}',
+        'line': {'color': '#fbc309'},
+        'data_labels': {
+            'font': {
+                'color': '#fbc309',
+                'size': 12,
+            },
+        },
+    })
+
+    chart.set_x_axis(config.x_axis)
+    chart.set_y_axis(config.y_axis)
+    chart.set_legend(config.legend)
+
+    chartsheet = workbook.add_chartsheet('Gráfico')
+    chartsheet.set_chart(chart)
+    
+
 
 
 if __name__ == '__main__':
