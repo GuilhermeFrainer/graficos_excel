2503c5a7734a2d9a565417858b7ff76eac7dced1
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-01-25 23:12:00 -0300

    Added IBC-Br to indice_volume

--- indice_volume/indice_volume.py
+++ indice_volume/indice_volume.py
@@ -1,6 +1,10 @@
 from sidrapy import get_table
 import sidra_helpers
 import xlsxwriter
+from datetime import date
+import requests
+import json
+import sys
 import config
 
 
@@ -8,18 +12,22 @@ def main():
     period = sidra_helpers.get_period(config.START_DATE)
     api_data = get_data(period)
     api_data = sidra_helpers.api_to_list(api_data)
-    headers = ['Mês', 'Varejo', 'Varejo Ampliado', 'Indústria', 'Serviços']
+    
+    data_list = get_ibc_br()
+    data_list = calculate_acc(data_list)
+    api_data.append(data_list)
+
+    headers = ['Mês', 'Varejo', 'Varejo Ampliado', 'Indústria', 'Serviços', 'IBC-Br']
     workbook, worksheet = sidra_helpers.make_excel(f'{config.FILE_PATH}Índice volume', api_data, headers)
     make_charts(workbook)
-
     credits = [
         'Arquivo feito em Python. Link do código:',
         'https://github.com/GuilhermeFrainer/graficos_excel',
-        'Fontes dos dados: tabelas 8185, 8186, 8159 e 8161 do SIDRA',
+        'Fontes dos dados: tabelas 8185, 8186, 8159 e 8161 do SIDRA e tabela 24364 da API do Bacen',
     ]
     sidra_helpers.make_credits(workbook, credits)
     workbook.close()
-    
+
 
 # Gets data from the Sidra API
 def get_data(period: str) -> list[list]:
@@ -122,6 +130,19 @@ def make_charts(workbook: xlsxwriter.Workbook) -> None:
             },
         },
     })
+    chart.add_series({
+        # IBC-Br
+        'name': '=Dados!$F$1',
+        'categories': f'=Dados!$A$2:$A${series_size + 2}',
+        'values': f'=Dados!$F$2:$F${series_size + 2}',
+        'line': {'color': '#7030a0'},
+        'data_labels': {
+            'font': {
+                'color': '#7030a0',
+                'size': 12,
+            },
+        },
+    })
 
     chart.set_x_axis(config.x_axis)
     chart.set_y_axis(config.y_axis)
@@ -131,6 +152,36 @@ def make_charts(workbook: xlsxwriter.Workbook) -> None:
     chartsheet.set_chart(chart)
     
 
+# Get two years before the start date
+# Needs to be done to calculate accumulated 12 month change for IBC-Br
+def get_previous_years(start_date: str) -> str:
+    date_list = start_date.split("-")
+    year = int(date_list[0]) - 2
+    return f"{year}-{date_list[1]}-{date_list[2]}"
+
+
+# Converts dates from their yyyy-mm-dd format in the config file to the dd/mm/yyyy used in the API
+def parse_date(iso_date: str) -> str:
+    date_list = iso_date.split("-")
+    return f"{date_list[2]}/{date_list[1]}/{date_list[0]}"
+
+
+# Gets IBC-Br data from the Bacen API
+def get_ibc_br() -> list[float]:
+    start_date = parse_date(get_previous_years(config.START_DATE))
+    end_date = parse_date(date.today().isoformat())
+    bacen_api_address = f'https://api.bcb.gov.br/dados/serie/bcdata.sgs.24364/dados?formato=json&dataInicial={start_date}&dataFinal={end_date}'
+    r = requests.get(bacen_api_address)
+    if r.status_code != 200:
+        sys.exit(f"Something went wrong at the Bacen API. Status code: {r.status_code}")
+
+    json_data = json.loads(r.text)
+    return [float(x['valor']) for x in json_data]
+
+
+# Gets the index from the json data and calculates accumulated 12-month change
+def calculate_acc(data_list: list[float]) -> list[float]:
+    return [((sum(data_list[i - 11:i + 1])/sum(data_list[i - 23:i - 11])) - 1) * 100 for i in range(24, len(data_list))]
 
 
 if __name__ == '__main__':
