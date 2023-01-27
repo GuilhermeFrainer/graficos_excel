ae4aa44f4c5be4a1fa2cea8f322fca61e07831b4
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-01-26 21:49:34 -0300

    Minor changes

--- caged/config.py
+++ caged/config.py
@@ -10,11 +10,12 @@ x_axis_config = {
 y_axis_config = {
     'num_font': {'name': 'Calibri', 'size': 12},
     'min': -1000000,
-    'max': 1000000,
+    'max': 1500000,
     'major_unit': 500000,
     'minor_unit': 100000,
     'major_gridlines': {'visible': False},
-    'num_format': '#,##0.0',
+    'display_units': 'thousands',
+    'display_units_visible': False,
 }
 
 y2_axis_config = {
@@ -23,7 +24,8 @@ y2_axis_config = {
     'max': 3200000,
     'major_unit': 1000000,
     'minor_unit': 200000,
-    'num_format': '#,##0.0',
+    'display_units': 'thousands',
+    'display_units_visible': False,
 }
 
 legend_config = {
